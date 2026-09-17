e7792fba6736b8e51de74a69ecb538b5ec3a9827
vietquang8071 <vitcongtu1708@gmail.com>
2026-09-17 11:56:35 +0700

    TfIdfVectorizer update

---
+++ lab1/TfIdfVectorizer.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import numpy as np
+import re
+from collections import Counter
+
+class TfIdfVectorizer():
+  def __init__(self, data:pd.DataFrame):
+    self.data = data
+
+    self.vocab = None
+    self.word_to_idx = None
+
+    self.count_matrix = None
+    self.tf = None
+    self.df = None
+    self.idf = None
+    self.tfidf = None
+
+  def buildVocabulary(self, col_name:str):
+    vocabulary = set()
+
+    for text in self.data[col_name]:
+      tokens = self._normalization(text).split()
+      vocabulary.update(tokens)
+
+    self.vocab = sorted(vocabulary)
+    self.word_to_idx = {
+      word: idx for idx, word in enumerate(self.vocab)
+    }
+
+  def _normalization(self, text: str):
+    return re.sub(r'[^\w\s]', '', text).lower()
+
+  def _compute_counts_for_each_text(self, text: str):
+    normalized_text = self._normalization(text)
+    tokens = normalized_text.split()
+    vector = np.zeros((len(self.vocab)), dtype=np.float32)
+
+    counts = Counter(tokens)
+    for token, count in counts.items():
+      idx = self.word_to_idx[token]
+      vector[idx] = count
+    return vector
+  
+  def compute_counts(self, col_name: str):
+    vectors = []
+    for text in self.data[col_name]:
+      vector = self._compute_counts_for_each_text(text)
+      vectors.append(vector)
+    self.count_matrix = np.array(vectors)
+
+  def compute_tf(self):
+    total_tokens = np.sum(self.count_matrix, axis=1, keepdims=True)
+    self.tf = np.divide(
+      self.count_matrix,
+      total_tokens,
+      out=np.zeros_like(self.count_matrix),
+      where=total_tokens != 0
+    )
+
+  def compute_df(self):
+    binary_matrix = self.count_matrix > 1
+    self.df = np.sum(binary_matrix, axis=0)
+
+  def compute_idf(self):
+    num_doc = self.count_matrix.shape[0]
+    self.idf = np.log((1 + num_doc) / (1 + self.df)) + 1
+
+  def compute_tfidf(self):
+    self.tfidf = self.tf * self.idf
+
+
